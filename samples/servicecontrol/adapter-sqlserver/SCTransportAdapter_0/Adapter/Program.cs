58393665272947d1e25effe4a9048fef6f5659ce
SzymonPobiega <szymon.pobiega@gmail.com>
2017-05-11 10:57:55 +0200

    Endpoints.
    Added the adapter.
    
    Markdown.
    
    Added a main doco page placeholder.
    
    Clean up
    
    Moved Contracts to Shared folder.
    
    Minor word tightening
    
    Hadi's review.
    
    Moved the sln to correct location.
    
    Database -> ChaosGenerator
    
    Removed forbidden word.
    
    Fixed urls
    
    Another URL fix.

---
+++ samples/servicecontrol/adapter-sqlserver/SCTransportAdapter_0/Adapter/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Configuration.AdvanceExtensibility;
+using NServiceBus.Routing;
+using NServiceBus.Transport.SQLServer;
+using ServiceControl.TransportAdapter;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Samples.ServiceControl.SqlServerTransportAdapter.Adapter";
+            AsyncMain().GetAwaiter().GetResult();
+        }
+
+        static async Task AsyncMain()
+        {
+            #region AdapterTransport
+
+            var adapterConfig = new TransportAdapterConfig<SqlServerTransport, SqlServerTransport>("ServiceControl.SqlServer.Adapter");
+
+            #endregion
+
+#pragma warning disable 618
+
+            #region EndpointSideConfig
+
+            adapterConfig.CustomizeEndpointTransport(transport =>
+            {
+                transport.EnableLegacyMultiInstanceMode(Connections.GetConnection);
+                //HACK: SQLServer expects this to be present. Will be solved in SQL 3.1
+                transport.GetSettings().Set<EndpointInstances>(new EndpointInstances());
+            });
+            
+            #endregion
+
+#pragma warning restore 618
+
+            #region SCSideConfig
+
+            adapterConfig.CustomizeServiceControlTransport(transport =>
+            {
+                transport.ConnectionString(
+                    @"Data Source=.\SQLEXPRESS;Initial Catalog=ServiceControl;Integrated Security=True;Max Pool Size=100;Min Pool Size=10");
+                //HACK: SQLServer expects this to be present. Will be solved in SQL 3.1
+                transport.GetSettings().Set<EndpointInstances>(new EndpointInstances());
+            });
+
+            #endregion
+
+            #region ControlQueueOverride
+
+            adapterConfig.ServiceControlSideControlQueue = "Particular.ServiceControl.SQL";
+            
+            #endregion
+
+            var adapter = TransportAdapter.Create(adapterConfig);
+
+            await adapter.Start();
+
+            Console.WriteLine("Press <enter> to shutdown adapter.");
+            Console.ReadLine();
+
+            await adapter.Stop();
+        }
+    }
+}
---
+++ samples/servicecontrol/adapter-sqlserver/SCTransportAdapter_0/Shipping/OrderAcceptedHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+class OrderAcceptedHandler : IHandleMessages<OrderAccepted>
+{
+    static ILog log = LogManager.GetLogger<OrderAcceptedHandler>();
+    ChaosGenerator chaos;
+
+    public OrderAcceptedHandler(ChaosGenerator chaos)
+    {
+        this.chaos = chaos;
+    }
+
+    public async Task Handle(OrderAccepted message, IMessageHandlerContext context)
+    {
+        log.Info($"Shipping order {message.OrderId} for {message.Value}");
+        await chaos.Invoke();
+        await context.Publish(new OrderShipped
+        {
+            OrderId = message.OrderId,
+            Value = message.Value
+        });
+    }
+}
